a6cd354f719edbf0e559039bc5a0c0bd98b8dcd5
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-02-09 01:18:46 -0600

    Create front page file

---
+++ src/components/FrontPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FrontPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+      </div>
+    );
+  }
+}
+
+export default FrontPage;

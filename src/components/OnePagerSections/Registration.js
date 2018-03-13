8d9823c4bd7a6b11e1aab7745f5b9cdf86bb7460
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-03-13 04:15:31 -0500

    Add content for registration and travel

--- src/components/OnePagerSections/Registration.js
+++ src/components/OnePagerSections/Registration.js
@@ -21,8 +21,17 @@ const Registration = () =>
   <div>
     <center>
       <Title>Registration</Title>
-      <Details>Information about registration (to be added)</Details>
     </center>
+      <Details>
+        <ul>
+          <li><p>To register visit this link (managed by xx)</p></li>
+          <li><p>After registration to
+          submit abstract visit this link (managed by organizers)</p></li>
+          <li><p>Early bird
+          registration ends on April 30. Regular registration on July 17</p></li>
+          <li><p>Registration fees cover: meals, attendance to sessions, coffee breaks and social activities.</p></li>
+        </ul>
+      </Details>
   </div>;
 
 export default Registration;

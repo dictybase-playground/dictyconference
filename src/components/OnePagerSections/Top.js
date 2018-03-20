83448ac204469a1865662d0aafe87a869d8b7423
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-03-20 02:58:58 -0500

    Add title over header photo

--- src/components/OnePagerSections/Top.js
+++ src/components/OnePagerSections/Top.js
@@ -8,19 +8,45 @@ import Button from "material-ui/Button";
 import HeaderImage from "../Images/header_image_4.jpg";
 
 const styles = theme => ({
-  background: {
-    background: "#011f4b",
-    }
-  })
+  container: {
+    position: "relative",
+  },
+  text: {
+    position: "absolute",
+    top: "30%",
+    textalign: "center",
+    color: "white"
+  },
+  title: {
+    fontSize: "5rem",
+    paddingLeft: "1px",
+    backgroundColor: "rgba(0,0,0, 0.5)",
+    paddingTop: "1%",
+    paddingBottom: "1%",
+  },
+  backgroundImage: {
+    filter: "brightness(80%)"
+  }
+});
 
-const Top = (props) =>
-  <div className={props.classes.background}>
-    <img src={HeaderImage} width="100%"/>
-    <h1>Dicty 2018</h1>
-    <h2>Annual International Dictyostelium Conference</h2>
-    <p>August 12 to August 16, 2018, The Netherlands</p>
-    <p>Egmond aan Zee</p>
+const Top = props =>
+  <div>
+    <center>
+      <div className={props.classes.container}>
+        <img
+          src={HeaderImage}
+          className={props.classes.backgroundImage}
+          width="100%"
+        />
+
+        <div className={props.classes.text}>
+            <h1 className={props.classes.title}>Dicty 2018</h1>
+        </div>
+      </div>
+    </center>
+    <p>Annual International Dictyostelium Conference</p>
+      <p>August 12 to August 16, 2018, The Netherlands</p>
+      <p>Egmond aan Zee</p>
   </div>;
-
 
 export default withStyles(styles)(Top);

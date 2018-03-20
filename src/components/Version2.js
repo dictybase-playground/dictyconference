28a8b191539f38c1aa619ed74f33470105d38125
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-03-19 20:49:35 -0500

    Header and navbar for version 2

--- src/components/Version2.js
+++ src/components/Version2.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
-import scrollToComponent from 'react-scroll-to-component';
+import scrollToComponent from "react-scroll-to-component";
+import headerImage from "./Images/header_image_3.jpg";
 
-import { Container, Container2, Container3, Division, Header, Title, SubTitle, Details, Button, NavBar, ArrowDown1, ArrowDown2, ArrowDown3 } from "./OnePageStyles";
+import {
+  Container,
+  Container2,
+  Container3,
+  Division,
+  Header,
+  Title,
+  SubTitle,
+  Details,
+  Button,
+  NavBar,
+  ArrowDown1,
+  ArrowDown2,
+  ArrowDown3
+} from "./Version2Styles";
 
-const Version2 = () => (
-    <div>
+const Version2 = () =>
+  <div>
+    <Container>
+      <img src={headerImage} width="100%" alt="Header Image" />
       <NavBar>
         <center>
           <Button onClick={() => scrollToComponent(this.Home, { offset: 0, align: 'top', duration: 1000})}>Home</Button>
@@ -15,60 +32,13 @@ const Version2 = () => (
           <Button onClick={() => scrollToComponent(this.Agenda, { offset: 0, align: 'top', duration: 1500})}>Agenda</Button>
         </center>
       </NavBar>
-      <Container>
-      </Container>
-      <Container className="Home" ref={(section) => { this.Home = section; }}>
-        <Header>
-          <Title>Dicty 2018</Title>
-          <SubTitle>Annual International Dictyostelium Conference</SubTitle>
-          <Details>August 12 to August 16, 2018, The Netherlands</Details>
-          <Details>Egmond aan Zee</Details>
-        </Header>
-      </Container>
-
-      <Container className="Venue" ref={(section) => { this.Venue = section; }}>
-
-        <center>
-          <Title>Venue</Title>
-          <Details>Information about venue (to be added)</Details>
-        </center>
-      </Container>
-
-
-      <Container2 dark className="Travel" ref={(section) => { this.Travel = section; }}>
-        <center>
-          <Container />
-          <Title>Travel</Title>
-          <Details>Information about venue (to be added)</Details>
-        </center>
-      </Container2>
-
-      <Container3 dark className="Registration" ref={(section) => { this.Registration = section; }}>
-        <center>
-          <Container />
-          <Title>Registration</Title>
-          <Details>Information about registration</Details>
-        </center>
-      </Container3>
-
-
-      <Container className="Abstract" ref={(section) => { this.Abstract = section; }}>
-        <center>
-          <Container />
-          <Title>Abstract</Title>
-          <Details>The abstract (to be added)</Details>
-        </center>
-      </Container>
-
-      <Container2 dark className="Agenda" ref={(section) => { this.Agenda = section; }}>
-        <center>
-          <Container />
-          <Title>Agenda</Title>
-          <Details>Information about agenda (to be added)</Details>
-        </center>
-      </Container2>
-
-    </div>
-)
+      <Header>
+        <Title>Dicty 2018</Title>
+        <SubTitle>Annual International Dictyostelium Conference</SubTitle>
+        <Details>August 12 to August 16, 2018, The Netherlands</Details>
+        <Details>Egmond aan Zee</Details>
+      </Header>
+    </Container>
+  </div>;
 
 export default Version2;
---
+++ src/components/Version2Styles.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import styled from "styled-components";
+import background from "./blue-background.jpg"
+
+
+export const Container = styled.div`
+  padding-top: 10%;
+  padding-bottom: 10%;
+  padding-left: 10%;
+  padding-right: 10%;
+  color: white;
+
+`
+
+export const Header = styled.p`
+  font-weight: lighter;
+  background-color: #011f4b;
+  text-align: center;
+
+`;
+
+export const Title = styled.p`
+  position: relative;
+  bottom: 0px;
+  font-size: 5em;
+  font-weight: lighter;
+`
+
+export const SubTitle = styled.h3`
+  position: relative;
+  bottom: 70px;
+  font-size: 2.5em;
+  font-weight: lighter;
+`
+
+export const Details = styled.h4`
+  position: relative;
+  bottom: 50px;
+  padding-left: 15%;
+  padding-right: 15%;
+  font-size: 1.1em;
+  font-weight: lighter;
+`
+
+export const NavBar = styled.div`
+  width: 100%;
+  overflow: hidden;
+  height: 64px;
+  z-index: 1;
+
+`
+
+export const Button = styled.button`
+
+  font-size: 1em;
+  font-weight: lighter;
+  margin: 5px; /* space between buttons */
+  padding: 10px 10px;
+  border: 2px solid black;
+  color: black;
+  border-radius: 3px;
+  background: none;
+
+
+  &:hover {
+    cursor: pointer;
+    background-color: #011f4b;
+    color: white;
+
+    /* Fade-in animation upon hover */
+    /* 3 lines to make it work on all browsers */
+    -webkit-transition:background-color 0.5s linear;
+    -moz-transition:background-color 0.5s linear;
+    -o-transition:background-color 0.5s linear;
+    transition:background-color 0.5s linear;
+
+  }
+
+`;

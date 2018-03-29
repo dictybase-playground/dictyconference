e990cc3d5b02b7e310f1a7b981c11424fb27be44
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-03-29 02:41:47 +0000

    version1 will be the default path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import OnePager from "./components/OnePager";
 import Version2 from "./components/Version2";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 require("typeface-roboto");
 
 
 const App = () =>
     <Switch>
+      <Redirect from="/" exact to="/version1" />
       <Route path="/version1" component={OnePager} />
       <Route path="/version2" component={Version2} />
     </Switch>;
